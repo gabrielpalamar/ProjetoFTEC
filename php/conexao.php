a7e6cd506e69d53cf9e937f60b1a2f3cc74adb8c
Cassio de Souza Cardoso <cassio.cardoso@sequor.com.br>
2023-04-26 01:47:21 -0300

    ajustado script de donexao e criação do banco, caso ele nao exista

--- php/conexao.php
+++ php/conexao.php
@@ -1,13 +1,26 @@
 <?php
 $db_file = __DIR__ . './../data/FTEC.sqlite';
-// Conecta ao banco de dados
-try {
-    $pdo = new PDO("sqlite:$db_file");
-} catch (PDOException $e) {
-    echo 'Erro ao conectar com o banco de dados: ' . $e->getMessage();
-    exit();
+
+// Verifica se o arquivo do banco de dados já existe
+if (!file_exists($db_file)) {
+    // Cria o banco de dados
+    try {
+        $pdo = new PDO("sqlite:$db_file");
+    } catch (PDOException $e) {
+        echo 'Erro ao conectar com o banco de dados: ' . $e->getMessage();
+        exit();
+    }
+
+    // Inclui o script de criação do banco de dados
+    include('/cria_banco.php');
+} else {
+    // Se o arquivo do banco de dados já existe, apenas conecta ao banco
+    try {
+        $pdo = new PDO("sqlite:$db_file");
+    } catch (PDOException $e) {
+        echo 'Erro ao conectar com o banco de dados: ' . $e->getMessage();
+        exit();
+    }
 }
 
-// Cria o banco de dados local, as tabelas e dados iniciais
-include('/cria_banco.php');
 ?>
--- php/cria_banco.php
+++ php/cria_banco.php
@@ -1,57 +1,103 @@
 <?php
-// Cria as tabelas caso não existam
+include_once('/conexao.php');
+
+#region Primeira tabela: tipos
 $pdo->exec("
--- Cria tabela de tipos
-CREATE TABLE tipos (
+CREATE TABLE IF NOT EXISTS tipos (
     tipo TEXT PRIMARY KEY
+)");
+// Array com os dados a serem inseridos
+$tipos = array(
+    "administrador",
+    "aluno",
+    "professor"
 );
 
--- Insere os tipos na tabela tipos caso não existam
-INSERT OR IGNORE INTO tipos (tipo) VALUES
-('administrador'),
-('aluno'),
-('professor');
-");
+// Loop para percorrer o array de tipos
+foreach($tipos as $tipo) {
+    // Verifica se o tipo já existe na tabela
+    $stmt = $pdo->prepare("SELECT tipo FROM tipos WHERE tipo = ?");
+    $stmt->execute([$tipo]);
+    $resultado = $stmt->fetch();
 
+    // Se o tipo não existir, insere na tabela
+    if(!$resultado) {
+        $stmt = $pdo->prepare("INSERT INTO tipos (tipo) VALUES (?)");
+        $stmt->execute([$tipo]);
+    }
+}
+
+#endregion
+
+#region Segunda tabela: materias
 $pdo->exec("
--- Cria tabela de matérias
-CREATE TABLE materias (
+CREATE TABLE IF NOT EXISTS materias (
     materia TEXT PRIMARY KEY
+)");
+
+// Array com os dados a serem inseridos
+$materias = array(
+    "Artes",
+    "Biologia",
+    "Educação Física",
+    "Filosofia",
+    "Física",
+    "Geografia",
+    "História",
+    "Inglês",
+    "Matemática",
+    "Português",
+    "Química",
+    "Sociologia"
 );
 
--- Insere as matérias do ensino médio na tabela materias caso não existam
-INSERT OR IGNORE INTO materias (materia) VALUES
-('Artes'),
-('Biologia'),
-('Educação Física'),
-('Filosofia'),
-('Física'),
-('Geografia'),
-('História'),
-('Inglês'),
-('Matemática'),
-('Português'),
-('Química'),
-('Sociologia');
-");
+// Loop para percorrer o array de materias
+foreach($materias as $materia) {
+    // Verifica se a materia já existe na tabela
+    $stmt = $pdo->prepare("SELECT materia FROM materias WHERE materia = ?");
+    $stmt->execute([$materia]);
+    $resultado = $stmt->fetch();
 
+    // Se a materia não existir, insere na tabela
+    if(!$resultado) {
+        $stmt = $pdo->prepare("INSERT INTO materias (materia) VALUES (?)");
+        $stmt->execute([$materia]);
+    }
+}
+#endregion
+
+#region Terceira tabela: usuarios
 $pdo->exec("
--- Cria tabela de usuários
-CREATE TABLE usuarios (
+CREATE TABLE IF NOT EXISTS usuarios (
     nome TEXT PRIMARY KEY,
+    email TEXT NOT NULL,
     senha TEXT NOT NULL,
     tipo TEXT NOT NULL,
     FOREIGN KEY (tipo) REFERENCES tipos(tipo)
-);
+)");
 
--- Insere usuario admin padrao na tabela usuarios caso não existam
-INSERT OR IGNORE INTO usuarios (nome, senha, tipo) VALUES
-('admin', 'admin', 'administrador');
-");
+// Dados a serem inseridos
+$nome = "Admin";
+$email = "admin@example.com";
+$senha = "admin";
+$tipo = "administrador";
 
+// Verifica se o usuário já existe na tabela
+$stmt = $pdo->prepare("SELECT nome FROM usuarios WHERE nome = ?");
+$stmt->execute([$nome]);
+$resultado = $stmt->fetch();
+
+// Se o usuário não existir, insere na tabela
+if(!$resultado) {
+    $stmt = $pdo->prepare("INSERT INTO usuarios (nome, email, senha, tipo) VALUES (?, ?, ?, ?)");
+    $stmt->execute([$nome, $email, $senha, $tipo]);
+}
+
+#endregion
+
+#region Quarta tabela: conteudos
 $pdo->exec("
--- Cria tabela de conteúdos
-CREATE TABLE conteudos (
+CREATE TABLE IF NOT EXISTS conteudos (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     titulo TEXT NOT NULL,
     conteudo TEXT NOT NULL,
@@ -59,6 +105,20 @@ CREATE TABLE conteudos (
     materia TEXT NOT NULL,
     FOREIGN KEY (nome) REFERENCES usuarios(nome),
     FOREIGN KEY (materia) REFERENCES materias(materia)
-);
-");
+)");
+#endregion
+
+#region Quinta tabela: eventos
+$pdo->exec("
+CREATE TABLE IF NOT EXISTS eventos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ano INTEGER NOT NULL,
+    mes INTEGER NOT NULL,
+    dia INTEGER NOT NULL,
+    titulo TEXT NOT NULL,
+    descricao TEXT NOT NULL,
+    criado_por TEXT NOT NULL
+)");
+#endregion
+
 ?>
