0dd855510ab794eb5ab3771d8c93799a08e41903
Cassio de Souza Cardoso <cassio.cardoso@sequor.com.br>
2023-05-06 23:58:47 -0300

    fix listagem de materias do aluno + add btn limpar

--- html/aluno/index.php
+++ html/aluno/index.php
@@ -1,7 +1,17 @@
 <?php
+include('../../php/conexao.php');
 session_start();
-include('../../php/lista_professores.php');
-include('../../php/lista_materias_aluno.php');
+
+//lista materias com conteudo cadastrado
+$stmt = $pdo->prepare('SELECT DISTINCT materia FROM conteudos');
+$stmt->execute();
+$materias = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//lista professores para email
+$stmt = $pdo->prepare("SELECT nome FROM usuarios WHERE tipo = 'professor'");
+$stmt->execute();
+$professores = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 if (isset($_GET['materia'])) {
     include('../../php/lista_conteudo.php');
 }
@@ -14,6 +24,7 @@ if (isset($_GET['materia'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="../../css/aluno.css">
     <link rel="stylesheet" href="../../css/geral.css">
 
@@ -24,26 +35,40 @@ if (isset($_GET['materia'])) {
 
 <body>
     <header>
-        <h1>
-            <?php echo 'Bem-vindo, ' . $_SESSION['usuario'] . '!'; ?>
-        </h1>
-        <form action="/php/logout.php" method="post">
-            <input type="submit" class="logout-bt" value="Logout">
-        </form>
-        <section>
-            <form action=# method="GET">
-                <select name="materia" id="materia">
-                    <?php foreach ($materias as $materia): ?>
-                        <option value="<?php echo $materia['materia']; ?>"><?php echo $materia['materia']; ?></option>
-                    <?php endforeach; ?>
-                </select><br>
-                <input type="submit" value="Buscar" class="buscaConteudo">
+        <div id="sessao-usuario">
+            <h1>
+                <?php echo 'Bem-vindo, ' . $_SESSION['usuario'] . '!'; ?>
+            </h1>
+            <form action="/php/busca.php" method="post" class="barra-pesquisa">
+                <input type="text" name="search" placeholder="Digite sua pesquisa...">
+                <button type="submit" class="buscaConteudo btn-search"><i class="material-icons">search</i></button>
             </form>
-        </section>
+
+            <form action="/php/logout.php" method="post">
+                <input type="submit" class="logout-bt" value="Logout">
+            </form>
+        </div>
+
     </header>
-    <main>
+    <section class="listaConteudos">
+        <form action="#" method="GET">
+            <select name="materia" id="materia">
+                <?php foreach ($materias as $materia): ?>
+                    <option value="<?php echo $materia['materia']; ?>"><?php echo $materia['materia']; ?></option>
+                <?php endforeach; ?>
+            </select>
+            <input type="submit" value="Buscar" class="buscaConteudo">
+            <br>
+            <button type="button" id="limpar" class="buscaConteudo">Limpar</button>
+        </form>
+
         <?php
         if (isset($_GET['materia'])) {
+            $materia = $_GET['materia'];
+            $stmt = $pdo->prepare("SELECT * FROM conteudos WHERE materia = :materia");
+            $stmt->bindParam(':materia', $materia);
+            $stmt->execute();
+
             while ($conteudos = $stmt->fetch(PDO::FETCH_ASSOC)) {
                 echo '<h3>' . $conteudos['titulo'] . '</h3>';
                 echo '<p>Professor: ' . $conteudos['nome'] . ' | ' . $conteudos['materia'] . '</p>';
@@ -51,16 +76,16 @@ if (isset($_GET['materia'])) {
                 echo '<hr>';
             }
         } else {
-            echo 'Selecione uma matéria para exibir os conteúdos.';
+            echo '<br>Selecione uma matéria para exibir os conteúdos.';
         }
         ?>
-    </main>
+    </section>
 
-    <form action="../../php/enviaEmail.php" method="post" class="formEmail">
-        <Label for="email_destinatario">Selecine o destinatario</Label>
-        <select name="email_destinatario" id="email_destinatario">
+    <form action="/../../php/enviaEmail.php" method="post" class="formEmail">
+        <Label for="prof">Selecione o destinatário</Label>
+        <select name="prof" id="prof">
             <?php foreach ($professores as $professor): ?>
-                <option value="<?php echo $professor['email']; ?>"><?php echo $professor['nome']; ?></option>
+                <option value="<?php echo $professor['nome']; ?>"><?php echo $professor['nome']; ?></option>
             <?php endforeach; ?>
         </select>
         <label for="mensagem">Mensagem</label><br>
@@ -69,12 +94,20 @@ if (isset($_GET['materia'])) {
         <input type="reset" name="BTApaga" value="Apagar" class="btnEmail">
     </form>
 
-
     <section class="eventos">
         <?php
         include('../../php/lista_eventos.php');
         ?>
     </section>
+    <script>
+        document.getElementById('limpar').addEventListener('click', function () {
+            const url = new URL(window.location.href);
+            url.searchParams.delete('materia');
+            window.location.href = url.href;
+        });
+    </script>
+
+
 </body>
 
 </html>
