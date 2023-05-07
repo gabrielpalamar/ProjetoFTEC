91bf4b036abf543ff85d35c4988874c057e5f6df
Gabriel Palamar <palamar_g@outlook.com>
2023-05-07 19:37:54 -0300

    ajustes_gp

--- html/admin/index.php
+++ html/admin/index.php
@@ -12,8 +12,14 @@ if (isset($_POST['mensagem'])) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <link rel="stylesheet" href="../../css/admin.css">
   <link rel="stylesheet" href="../../css/geral.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <title>
     <?php echo $_SESSION['usuario']; ?>
   </title>
@@ -21,8 +27,21 @@ if (isset($_POST['mensagem'])) {
 
 <body>
   <header>
-    <?php include '../header.php' ?>
+  
+  <div id="sessao-usuario">
+            <img class="logo-header" id="logo" src="../../css/assets/research.svg" alt="Lampada " />
+            <div>
+            <span class="span-title">
+                <?php echo 'Bem-vindo, ' . $_SESSION['usuario'] . '!'; ?>
+            </span>
+  </div>    
+            <form action="/php/logout.php" method="post">
+                    <input type="submit" class="logout-bt" value="Logout">
+                </form>
+            </div>
+            
   </header>
+  
   <main class="form">
     <h2>Cadastro de Usuário</h2>
     <form method="post" action="../../php/criar_usuario.php">
--- html/prof/index.php
+++ html/prof/index.php
@@ -17,9 +17,15 @@ $materias = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="../../css/prof.css">
     <link rel="stylesheet" href="../../css/geral.css">
     <link rel="stylesheet" href="../../css/textEditor.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>
         <?php echo $_SESSION['usuario']; ?>
     </title>
@@ -27,10 +33,21 @@ $materias = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <body>
     <header>
-        <?php include '../header.php' ?>
+    <div id="sessao-usuario">
+            <img class="logo-header" id="logo" src="../../css/assets/research.svg" alt="Lampada " />
+
+                <form action="/php/logout.php" method="post">
+                    <input type="submit" class="logout-bt" value="Logout">
+                </form>
+            </div>
     </header>
     <main>
         <section class="section">
+        <div>
+            <span class="span-title">
+                <?php echo 'Bem-vindo, ' . $_SESSION['usuario'] . '!'; ?>
+            </span>
+        </div>
             <form action='/../../php/criar_conteudo.php' method="post">
                 <h2>Cadastrar um novo conteudo</h2><br>
                 <select name="materia" id="materia" required>
@@ -38,12 +55,16 @@ $materias = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         <option value="<?php echo $materia['materia']; ?>"><?php echo $materia['materia']; ?></option>
                     <?php endforeach; ?>
                 </select>
+                <div>
                 <label for="titulo">Titulo</label><br>
                 <input type="text" name="titulo" value=""><br>
+                </div>
+                <div>
                 <label for="conteudo">Conteúdo</label><br>
                 <div id="container">
                     <textarea id="editor" name="conteudo">
                     </textarea>
+                </div>  
                 </div>
 
                 <input type="submit" value="Cadastrar Conteúdo" class="cadConteudo">
@@ -51,15 +72,22 @@ $materias = $stmt->fetchAll(PDO::FETCH_ASSOC);
         </section>
         <section class="section">
             <form action="/../../php/criar_evento.php" method="post">
-                <label for="dataDoEvento">Data:</label>
+                
+                <div>
+                <label for="dataDoEvento">Data:</label><br>
                 <input type="date" id="data" name="dataDoEvento">
+                </div>
 
-                <label for="tituloDoEvento">Titulo do Evento</label>
+                <div>
+                <label for="tituloDoEvento">Titulo do Evento</label><br>
                 <input type="text" name="tituloDoEvento">
+                </div>
 
-                <label for="descricaoDoEvento">Descrição do Evento</label>
+                <div>
+                <label for="descricaoDoEvento">Descrição do Evento</label><br>
                 <textarea name="descricaoDoEvento"></textarea>
-
+                </div>
+                
                 <input type="submit" value="Cadastrar Evento" class="cadEvento">
             </form>
         </section>
